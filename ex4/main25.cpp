902d7f64f179b98265d410bd9bea3b7efb3523dd
Rebecca <79047845+rebeccahou0424@users.noreply.github.com>
2025-04-10 15:33:20 +0800

    Update main25.cpp

--- ex4/main25.cpp
+++ ex4/main25.cpp
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
-void printRow(int i, int j){
-    if(i>9) return;
-    if(j>9){
-        printf("\n");
-        printRow(i+1, 1);
-        return;
-    }
-    printf("%d*%d=%2d ", i, j, i*j);
-    printRow(i, j+1);
+int my_strlen(char* s){
+        if(!*s) return 0;
+        return 1+ my_strlen(s+1);
 }
 
 int main()
 {
-    printRow(1, 1);
+    char s[] = "IU is a girl!";
+    printf("(%s) len = %d\n", s, my_strlen(s));
     return 0;
 }
+  
